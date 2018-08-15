0a2d889fc37e5aff682d3c171372b0cb2274d1ff
Iamhereing <1161564825@qq.com>
2018-08-15 20:26:19 +0800

    my first code

---
+++ baidu_logo.py
@@ -0,0 +1,12 @@
+import requests
+
+if __name__ == '__main__':
+    target = 'http://www.baidu.com/img/bd_logo1.png?qua=high&where=super'
+    rsp = requests.get(url=target)
+
+    with open('a.png',"wb") as f:
+        f.write(rsp.content)
+    print(rsp.text)
+
+
+
